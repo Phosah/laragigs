50258d4b1b73e7ac04bae5f12014b6c8066018cf
Phosah <efosa.uyi@gmail.com>
2023-03-11 13:21:48 +0100

    Implementation of Edit and Delete functions

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,16 @@ Route::get('/listings/create', [ListingController::class, 'create' ]);
 // /store listings
 Route::post('/listings', [ListingController::class, 'store' ]);
 
-//Single Listing
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Update Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+//Delete Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 // Alternative Single Listing
